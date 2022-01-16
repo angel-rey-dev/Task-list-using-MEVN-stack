8395570f1a27e4f600605e012e9a66b2293017b9
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-16 07:22:15 -0400

    Update index.ts

--- 2-Intermediate/CRUD-using-MEVN-stack/backend/routes/index.ts
+++ 2-Intermediate/CRUD-using-MEVN-stack/backend/routes/index.ts
@@ -4,24 +4,54 @@ import Task from "../models/Task";
 const router = Router();
 
 router.get("/tasks", async (req, res) => {
-  const tasks = await Task.find();
-  res.send("Get all tasks: " + JSON.stringify(tasks));
+  try {
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.status(200).json("/tasks: " + JSON.stringify(tasks));
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-router.get("/tasks/:id", (req, res) => {
-  res.send("Get task by id");
+router.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).send("Task not found");
+    res.status(200).json("/task/:id " + JSON.stringify(task));
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 router.post("/tasks", (req, res) => {
-  res.send("Upload a task");
+  try {
+    const task = new Task(req.body);
+    task.save();
+    res.status(201).json("post task: " + task);
+  } catch (error) {}
 });
 
-router.put("/tasks", (req, res) => {
-  res.send("Update task");
+router.put("/tasks/:id", async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedTask) return res.status(404).send("Task not found");
+    res.status(200).json("put task: " + updatedTask);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-router.delete("/tasks/:id", (req, res) => {
-  res.send("Delete task");
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).send("Task not found");
+    res.status(200).json("delete task: " + task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 export default router;
