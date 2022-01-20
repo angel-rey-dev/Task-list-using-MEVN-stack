5af09f9430905419309bac9a8793611bbfa5a8b7
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-20 17:16:26 -0400

    Update axios.ts

--- 1-Basics/Task-list-using-MEVN-stack/frontend/src/services/axios.ts
+++ 1-Basics/Task-list-using-MEVN-stack/frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://task-list-mevn.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
   },
