0c840470c437948790c75ddcfadcd34e8dfe05e4
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-20 17:10:42 -0400

    Update database.ts

--- 1-Basics/Task-list-using-MEVN-stack/backend/database.ts
+++ 1-Basics/Task-list-using-MEVN-stack/backend/database.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 export const startConnection = async () => {
   try {
-    const db = await connect("mongodb+srv://jstyles:001225@mevncrud.qyc8d.mongodb.net/mevnCRUD?retryWrites=true&w=majority");
+    const db = await connect("mongodb+srv://jstyles:001225@mevncrud.qyc8d.mongodb.net/mevncrud?retryWrites=true&w=majority");
     console.log("Database is connected : ", db.connection.name);
   } catch (error) {
     console.log(error);
