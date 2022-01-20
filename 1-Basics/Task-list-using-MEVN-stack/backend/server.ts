b78255785f10e4298f1c9bd2bff50c3dfc32de13
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-20 17:46:13 -0400

    Update server.ts

--- 1-Basics/Task-list-using-MEVN-stack/backend/server.ts
+++ 1-Basics/Task-list-using-MEVN-stack/backend/server.ts
@@ -5,13 +5,15 @@ import morgan from "morgan";
 import taskRoutes from "./routes";
 
 const server = express();
-server.use(cors());
+server.use(cors( 
+    {
+        origin: "https://cool-task-list.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }
+));
 server.use(morgan("dev"));
 server.use(express.json());
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://cool-task-list.vercel.app/');
-});
 
 server.use("/api", taskRoutes);
 
